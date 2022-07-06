c62f682e9d0f45ac4850cc1f214d985d5f83de91
loguchiyan <108789418+loguchiyan@users.noreply.github.com>
2022-07-06 17:39:03 +0530

    Add files via upload

---
+++ src/app/app.component.ts
@@ -0,0 +1,51 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+
+  exform:any;
+
+  ngOnInit() {
+
+  this.exform = new FormGroup({
+    'name' : new FormControl(null, Validators.required),
+    'email' : new FormControl(null, [Validators.required, Validators.email]),
+    'password' : new FormControl(null, Validators.required),
+  
+    'phone' : new FormControl(
+      null,
+      [
+        Validators.required,
+        Validators.pattern('^\\s*(?:\\+?(\\d{1,3}))?[-. (]*(\\d{3})[-. )]*(\\d{3})[-. ]*(\\d{4})(?: *x(\\d+))?\\s*$')
+      ]),
+    'message' : new FormControl(null, [Validators.required, Validators.minLength(10)])
+  });
+  }
+
+  clicksub() {
+    console.log(this.exform.value);
+    this.exform.reset();
+  }
+  get name() {
+    return this.exform.get('name');
+  }
+  get email() {
+    return this.exform.get('email');
+  }
+  get password() {
+    return this.exform.get('password');
+  }
+ 
+  get phone() {
+    return this.exform.get('phone');
+  }
+  get message() {
+    return this.exform.get('message');
+  }
+
+}
